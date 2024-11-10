5fe20cbb802f83d3d61bc6ad88eb257998bd96f7
sumana-sri-k <94799091+sumana-sri-k@users.noreply.github.com>
2024-11-10 22:45:23 +0530

    Create valid_string.java

---
+++ valid_string.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(check(str)?"YES":"NO");
+	}
+    public static boolean check(String str){
+        if (str.length()==0){
+            return true;
+        }
+
+        if(str.charAt(0)=='a'){
+            if(str.length()==1 || str.length()>=2 && str.charAt(1)=='a'){
+                return check(str.substring(1));
+            }
+            else if(str.length()>=3 && str.charAt(1)=='b' && str.charAt(2)=='b'){
+                return check(str.substring(3));
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
